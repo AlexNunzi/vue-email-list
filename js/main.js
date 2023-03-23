4ba03e87503ff074da2e13c9c3d100af53959518
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-23 15:23:02 +0100

    Generazione tramite API di Boolean 10 indirizzi email e stampa a schermo all'interno di una lista

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      
+    data() {
+        return {
+            mailList: []
+        }
+    },
+    methods:{
+        generateMail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                    this.mailList.push(response.data.response);
+                }
+            );
+        }
+    },
+    mounted(){
+        for(i = 0; i < 10; i++){
+            this.generateMail();
+        }
     }
-  }
 }).mount('#app')
